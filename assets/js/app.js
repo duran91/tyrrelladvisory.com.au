1146c5c09c4d3bd47b3d674e853b1e92e465902d
matthew <matt.duran@rea-group.com>
2019-07-08 22:24:39 +1000

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,36 @@ $(function() {
   });
 
 
+
   $(".parent-mobile-link").on("click", function(){
+    console.log("made it")
     $(this).siblings(".mobile-dropdown").toggleClass("hidden");
+  })
+
+  $("#email-form").submit(function(e){
+    e.preventDefault()
+
+    var $form = $(this);
+    var serializedData = $form.serialize();
+
+    request = $.ajax({
+      url: "/tyrrell_email.php",
+      type: "post",
+      data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+      // Log a message to the console
+      console.log("Hooray, it worked!");
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+      // Log the error to the console
+      console.error(
+          "The following error occurred: "+
+          textStatus, errorThrown
+      );
+    });
   })
 });
